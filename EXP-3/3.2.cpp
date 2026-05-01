e2cf9a6f0e55edcea85536153ce1af3209f88b22
prathameshbidkar98-sys <prathameshbidkar98@gmail.com>
2026-05-01 14:35:12 +0530

    Create 3.2.cpp

---
+++ EXP-3/3.2.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+
+void printNto1(int n) {
+    if (n <= 0) return;
+    cout << n << " ";
+    printNto1(n - 1);
+}
+
+int main() {
+    int N = 10;
+    printNto1(N);
+    cout << endl;
+    return 0;
+}
