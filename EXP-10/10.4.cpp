b576a247f4e76984453d9ffcddcb277505d44ee1
prathameshbidkar98-sys <prathameshbidkar98@gmail.com>
2026-05-01 14:52:31 +0530

    Create 10.4.cpp

---
+++ EXP-10/10.4.cpp
@@ -0,0 +1,16 @@
+#include <queue>
+
+using namespace std;
+
+int main() {
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    
+    cout << "Front: " << q.front() << endl;
+    q.pop();
+    cout << "Front after pop: " << q.front() << endl;
+    
+    return 0;
+}
